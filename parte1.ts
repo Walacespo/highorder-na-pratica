d9d13151e99d20891fe64ff8a322c28d00e7ca3b
Walace da Silva <walacespo@gmail.com>
2022-09-15 13:57:09 -0300

    FILTER

--- parte1.ts
+++ parte1.ts
@@ -23,3 +23,20 @@ console.log('MAP ------------------------------');
 console.log(m1);
 console.log(m2);
 console.log(m3);
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// filter: retorna uma nova coleção contendo apenas aqueles elementos da coleção original que satisfazem um dado predicado
+
+function par(x: number) : boolean {
+    return x % 2 == 0;
+}
+
+let f1 = list1.filter(par);
+let f2 = list1.filter(x => x > 2);
+let f3 = list1.filter(x => x % 2 != 0);
+
+console.log('FILTER ------------------------------');
+console.log(f1);
+console.log(f2);
+console.log(f3);
