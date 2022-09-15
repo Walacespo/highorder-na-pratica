b3fab8d3ef665a9630e74c8069c67af0ad1c7f9e
Walace da Silva <walacespo@gmail.com>
2022-09-15 13:55:47 -0300

    MAP

---
+++ parte1.ts
@@ -0,0 +1,25 @@
+export {}
+
+let list1 = [1, 2, 3, 4];
+let list2 = [];
+let nomes = ['MARIA', 'JOAO', 'ANABELA'];
+
+// --------------------------------------------------------------------------------------------------------------------
+// map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção com os elementos alterados
+
+function dobro(x: number) : number {
+    return x * 2;
+}
+
+function triplo(x: number) : number {
+    return x * 3;
+}
+
+let m1 = list1.map(dobro);
+let m2 = list1.map(triplo);
+let m3 = list1.map(x => 2 * x);
+
+console.log('MAP ------------------------------');
+console.log(m1);
+console.log(m2);
+console.log(m3);
