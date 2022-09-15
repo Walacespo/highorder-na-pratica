ce17427dbeb8879814f54883d798827ccf8c72b0
Walace da Silva <walacespo@gmail.com>
2022-09-15 13:58:49 -0300

    REDUCE

--- parte1.ts
+++ parte1.ts
@@ -40,3 +40,29 @@ console.log('FILTER ------------------------------');
 console.log(f1);
 console.log(f2);
 console.log(f3);
+
+// --------------------------------------------------------------------------------------------------------------------
+// reduce: aplica cumulativamente uma função aos elementos de uma coleção, retornando o resultado final.
+// * você pode informar, opcionalmente, um valor inicial como parâmetro (necessário para coleção vazia).
+
+function soma(x : number, y : number) : number {
+    return x + y;
+}
+
+function produto(x : number, y : number) : number {
+    return x * y;
+}
+
+let r1 = list1.reduce(soma);
+let r2 = list1.reduce(produto);
+
+//let r3 = list2.reduce(soma);
+let r4 = list2.reduce(soma, 0);
+let r5 = list2.reduce(produto, 1);
+
+console.log('REDUCE ------------------------------');
+console.log(r1);
+console.log(r2);
+console.log(r4);
+console.log(r5);
+
